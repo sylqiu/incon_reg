b86af96a9282ae85b75d6c176eaee2f29dd49613
sylqiu <sylvesterqiu@gmail.com>
2019-12-04 00:50:43 +0800

    curvature bug fix

--- utils/load_shape_obj.m
+++ utils/load_shape_obj.m
@@ -2,8 +2,5 @@ function [face, vertex, gauss_curvature, mean_curvature] = load_shape_obj(obj_pa
 disp('loading obj with format from Gabriel Peyr')
 [vertex, face] = readOBJ(obj_path);
 [face, vertex] = clean_mesh(face, vertex);
-gauss_curvature = discrete_gaussian_curvature(vertex, face);
-L = cotmatrix(vertex,face);
-mean_curvature = L*vertex;
-mean_curvature = real(sqrt(sum(mean_curvature.^2,2)));
+[~, ~, ~, ~, gauss_curvature, mean_curvature,~] = compute_curvature(vertex, face);
 end
